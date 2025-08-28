65bf22d442b2e8390bf6e84d5ac0d7bd6bb35d22
TrashLoaded <alexiseba15@gmail.com>
2025-08-28 00:42:38 -0400

    Alerta y Medicion

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -11,3 +11,19 @@ class Dispositivo(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Medicion(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    consumo = models.DecimalField(max_digits=10, decimal_places=3)
+    tomada_en = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"{self.dispositivo} - {self.consumo}"
+
+class Alerta(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo,on_delete=models.CASCADE)
+    mensaje = models.CharField(max_length=150)
+    creada_en = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"{self.dispositivo} - {self.mensaje}"
