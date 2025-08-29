dc2e07ba24cad0151b20cebb8935c2834548a3e8
SuN-Dev5 <martin.cifuentes@inacapmail.cl>
2025-08-28 22:07:58 -0400

    Cambio totalmente necesario

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Dispositivo(models.Model):
-    nombre = models.CharField(max_length=100)
-    categoria = models.ForeignKey("Categoria", on_delete= models.CASCADE)
-    zona = models.ForeignKey("Zona", on_delete=models.CASCADE)
-    estado = models.BooleanField(default=True)
-    prim_consumo = models.DecimalField(max_digits=10, decimal_places=3)
-
-
-    def __str__(self):
-        return self.nombre
