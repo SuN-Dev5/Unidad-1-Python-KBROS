e30dea910e342d9a827cc065beceb90394d6eead
TrashLoaded <alexiseba15@gmail.com>
2025-08-28 22:26:34 -0400

    Arreglo Creacion de Medicion y Alerta

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Dispositivo(models.Model):
-    nombre = models.CharField(max_length=100)
-    categoria = models.ForeignKey("Categoria", on_delete= models.CASCADE)
-    zona = models.ForeignKey("Zona", on_delete=models.CASCADE)
-    estado = models.BooleanField(default=True)
-    prim_consumo = models.DecimalField(max_digits=10, decimal_places=3)
-
-
-    def __str__(self):
-        return self.nombre
-
 class Medicion(models.Model):
     dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
     consumo = models.DecimalField(max_digits=10, decimal_places=3)
