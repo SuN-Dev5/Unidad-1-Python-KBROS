b8c778f19d4554590685ede2e54ecee6b8b5c6a3
SuN-Dev5 <martin.cifuentes03@inacapmail.cl>
2025-08-28 23:11:36 -0400

    no me pegue profe models.py
    💀

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=True)
+    prim_consumo = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self):
+        return self.nombre
+
  feature/modelos_alerta_medicion_Seba
 # Create your models here.
 class Medicion(models.Model):
