7c62c8dc311f0342713c224e1840827e9fc9f17f
SuN-Dev5 <martin.cifuentes03@inacapmail.cl>
2025-09-11 03:20:53 -0300

    Revert "Se creo en el login recuperar contraseña"

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -125,16 +125,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Para desarrollo (emails en consola)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# O para producción con Gmail
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'tu-email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'tu-app-password'
-
-DEFAULT_FROM_EMAIL = 'noreply@ecoenergy.com'
